93a52b3edfabf4fda8049308a7c01b4a715e46cd
Southpika <513923576@qq.com>
2024-04-07 08:02:12 +0000

    temp save for hf pt/sft

--- finetune_hf.py
+++ finetune_hf.py
@@ -1,78 +1,98 @@
+import deepspeed
+# deepspeed.ops.op_builder.CPUAdamBuilder().load()
+deepspeed.init_distributed()
 
+import os
 import torch
 import transformers
-from accelerate import Accelerator
+import time
+# from accelerate import Accelerator, PartialState
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
 from trl import SFTTrainer
+import logging
 
-from accelerate import PartialState
-device_string = PartialState().process_index 
-accelerator = Accelerator(
-    cpu=None, mixed_precision=None, log_with="all", project_dir="./output/log"
-)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(message)s')
+
+def get_logger(path):
+    log_file = os.path.join(path, 'logfile.log')
+    logger = logging.getLogger('my_logger')
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(message)s')
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+    return logger
+
+# device_string = PartialState().process_index 
+# accelerator = Accelerator(
+#     cpu=None, mixed_precision=None, log_with="all", project_dir="./output/log"
+# )
 
 model_id = "google/gemma-2b"
-
-
-# Load the pretrained model and tokenizer.
 tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16, device_map={'':device_string})
-# model.to(accelerator.device)
-# model.is_parallelizable = True
-# model.model_parallel = True
+model = AutoModelForCausalLM.from_pretrained(model_id, torch_dtype=torch.float16)
 print(model.device)
-# breakpoint()
-# Load the dataset and format it for training.
+# Load the dataset and format it for training. , device_map={'':device_string}
 data = load_dataset("/root/tzh_workspace/PaddleNLP/llm/data/")
 def tokenize_function(example):
-    return tokenizer(example["src"], padding="max_length", truncation=True)
-train_dataset = data["train"].map(tokenize_function, batched=True)
+    res = [example["src"][i]+example["tgt"][i] for i in range(len(example["src"]))]
+    return tokenizer(res, padding="max_length", truncation=True, max_length=1024)
+
+train_dataset = data["train"].select(range(1000)).map(tokenize_function, batched=True)
 eval_dataset = data["validation"].map(tokenize_function, batched=True)
-trainer = SFTTrainer(
+
+print(train_dataset[0]["input_ids"].__len__())
+
+# trainer = SFTTrainer(
+#     model=model,
+#     train_dataset=train_dataset,
+#     eval_dataset=eval_dataset,
+#     dataset_text_field="src",
+#     packing=False,
+#     max_seq_length = 256,
+#     args=TrainingArguments(
+#         per_device_train_batch_size=2,
+#         gradient_accumulation_steps=8,
+#         warmup_steps=10,
+#         num_train_epochs=1,
+#         learning_rate=3e-6,
+#         fp16=True,
+#         logging_steps=1,
+#         logging_dir='./output',
+#         optim="adamw_torch",
+#         evaluation_strategy="epoch",
+#         save_total_limit=3,
+#         output_dir='./output',
+#         gradient_checkpointing=True,
+#         lr_scheduler_type="linear", 
+#         warmup_ratio=0.1, 
+#     )
+# )
+trainer = Trainer(
     model=model,
     train_dataset=train_dataset,
     eval_dataset=eval_dataset,
-    dataset_text_field="src",
-    packing=True,
-    args=TrainingArguments(
-        # fsdp_config=fsdp_config,
+    args=transformers.TrainingArguments(
         per_device_train_batch_size=1,
-        gradient_accumulation_steps=4,
-        warmup_steps=30,
-        num_train_epochs=1,
-        learning_rate=3e-6,
+        gradient_accumulation_steps=8,
+        optim="adamw_torch", 
+        logging_steps=1, 
+        learning_rate=3e-5, 
         fp16=True,
-        logging_steps=1,
-        optim="adamw_torch",
-        evaluation_strategy="steps",
-        eval_steps=10,
-        save_total_limit=3,
+        lr_scheduler_type="linear", 
+        max_steps=50,
+        save_strategy="steps", 
         output_dir='./output',
-        # fsdp="full_shard",
-    )
+        gradient_checkpointing=False,
+        fp16_opt_level="O2",
+        deepspeed="/root/tzh_workspace/PaddleNLP/ds_config_stage3.json"
+    ),
+    data_collator=DataCollatorForLanguageModeling(tokenizer=tokenizer, mlm=False),
 )
-# trainer = transformers.Trainer(
-#     model=model,
-#     train_dataset=train_dataset,
-#     args=transformers.TrainingArguments(
-#         full_shard=True,
-#         shard_grad_op=3,
-#         per_device_train_batch_size=4,
-#         gradient_accumulation_steps=16,
-#         warmup_steps=30,
-#         num_train_epochs=1,
-#         learning_rate=3e-6,
-#         # fp16=True,
-#         logging_steps=1,
-#         optim="adamw_torch",
-#         evaluation_strategy="steps",
-#         eval_steps=10,
-#         save_total_limit=3,
-#         output_dir='./output'
-#     ),
-#     data_collator=transformers.DataCollatorForSeq2Seq(
-#             tokenizer,return_tensors="pt", padding=True
-#         ),
-# )
+logger = get_logger("/root/tzh_workspace/use_time")
+cur_time = time.time()
+logger.info(f"[Start of Training if HF]{cur_time}, use data size {len(train_dataset)}")
 trainer.train()
+end_time = time.time()
+logger.info(f"[End of Training if HF]{cur_time}, use {end_time-cur_time} s")
